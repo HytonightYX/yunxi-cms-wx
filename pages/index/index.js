0c6718c0698a868746e0f37fa087a6d8fedc0d63
HytonightYX <whosiyuan@qq.com>
2019-07-05 23:43:36 +0800

    申请token、保存token、请求最新期刊
    申请token、保存token、请求最新期刊

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
+import {Base64} from 'js-base64'
+
 Page({
   onGetToken() {
-    // code
     wx.login({
       success: res => {
         if (res.code) {
@@ -12,7 +13,11 @@ Page({
               type: 100
             },
             success: res => {
-              console.log 
+              console.log(res.data)
+              wx.setStorage({
+                key: "token",
+                data: res.data.token
+              })
             }
           })
         }
@@ -21,11 +26,23 @@ Page({
   },
   onGetLatest() {
     wx.request({
-      url: 'http://localhost:3030/v1/latest',
+      url: 'http://localhost:3030/v1/classic/latest',
       method: 'GET',
       success: res => {
         console.log(res)
-      }
+      },
+      header: {
+        Authorization: this._encode()
+      },
     })
+  },
+  // 手动进行base64加密
+  _encode() {
+    const token = wx.getStorageSync('token')
+    console.log(token)
+    // 格式：Authorization: "Basic username:password"
+    // 现在username 就是token，password为空
+    const base64 = Base64.encode(token + ':' + '')
+    return 'Basic ' + base64
   }
 })
